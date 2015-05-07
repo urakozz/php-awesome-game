c9f9f99e34d285c58382833ccb23d63eb0be9760
Yury Kozyrev <urakozz@me.com>
2015-05-07 16:22:08 +0200

    Create Game.php

---
+++ src/Misc/Game.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kozz\Misc
+
+class Game {
+
+  public static function getInstance(){
+    static $instance;
+    return $instance ?: $instance = new static;
+  }
+  
+  public function __construct(){
+  
+  }
+  
+  public funciton play(){
+    echo "\nPHP Awesome Game\n\n";
+    echo "Guess number between 1 and 10!\n> ";
+    $random = rand(1, 10);
+    $line   = (int) trim(fgets(STDIN));
+    echo $random === $line
+        ? "Yes, you are lucky one!"
+        : sprintf("No, i guessed %d", $random);
+    echo "\n";
+  }
+}
