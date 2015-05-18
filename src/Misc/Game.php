8d7f372568eda0a2ce714cea5d5e222b10ee6f8c
Yury Kozyrev <urakozz@me.com>
2015-05-18 10:53:12 +0200

    Update Game.php

--- src/Misc/Game.php
+++ src/Misc/Game.php
@@ -3,15 +3,30 @@
 namespace Kozz\Misc;
 
 class Game {
+  protected $boss;
+  
+  public function __construct(){
+    $isBoss = !empty(getopt('', ['basePath::']));
+    $this->setBoss($isBoss);
+  }
+  
+  public function setBoss($boss){
+    $this->boss = $boss;
+  }
 
   public function play(){
     echo "\nPHP Awesome Game\n\n";
     echo "Guess number between 1 and 10!\n> ";
-    $random = rand(1, 10);
+    $random = $this->getRandom();
     $line   = (int) trim(fgets(STDIN));
     echo $random === $line
         ? "Yes, you are lucky one!"
         : sprintf("No, i guessed %d", $random);
     echo "\n";
+  }
+  
+  protected function getRandom(){
+    $last = $this->boss ? 100 : 10;
+    $random = rand(1, $last);
   }
 }
